d56e46f353a9174a18d918bdc87558e036e5ca2e
Yehonathan Sharvit <yehonathan@kontera.com>
2012-07-26 12:27:19 +0300

    install.sh: override even if file exists

--- install.sh
+++ install.sh
@@ -2,12 +2,8 @@
 repository_folder=`pwd`
 echo $repository_folder
 cd $HOME
-for x in .tmux.conf .zshrc .bashrc .vimrc .screenrc .inputrc .vim .gitignore bin .clj_completions find_javascript_all_files; do 
-    if [ -a $x ]; then
-        echo "please remove $x"
-    else 
-        y="ln -s $repository_folder/$x $x"
-        echo $y
-        `$y`
-    fi;
+for x in .tmux.conf .zshrc .bashrc .vimrc .screenrc .inputrc .vim .gitignore_global bin .clj_completions find_javascript_all_files; do 
+   y="ln -f -s $repository_folder/$x $x"
+   echo $y
+   `$y`
 done
