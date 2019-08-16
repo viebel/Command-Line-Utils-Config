7b9622395961016ddb7c78b640e720c1775a8b47
Yehonathan Sharvit <viebel@gmail.com>
2019-08-16 08:27:09 +0300

    install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ repository_folder=`pwd`
 echo $repository_folder
 cd $HOME
 rm .screenrc .inputrc .bashrc .vimrc .vim .zshrc bin
-for x in .emacs .tmux.conf .zshrc .bashrc .screenrc .inputrc .vim .gitignore_global bin; do 
+for x in .spacemacs.d .tmux.conf .zshrc .bashrc .screenrc .inputrc .vim .gitignore_global bin; do 
    y="ln -f -s $repository_folder/$x $x"
    echo $y
    `$y`
