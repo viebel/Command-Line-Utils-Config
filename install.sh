3d724ec97024cd10d0d4017557417a17b51c2953
Yehonathan Sharvit <viebel@gmail.com>
2019-08-16 08:20:53 +0300

    spacemacs config

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 repository_folder=`pwd`
 echo $repository_folder
 cd $HOME
-rm .screenrc .inputrc .bashrc .vimrc .vim .zshrc ./.clojure/deps.edn bin
-for x in .emacs .tmux.conf .zshrc .bashrc .vimrc .screenrc .inputrc .vim .gitignore_global bin; do 
+rm .screenrc .inputrc .bashrc .vimrc .vim .zshrc bin
+for x in .emacs .tmux.conf .zshrc .bashrc .screenrc .inputrc .vim .gitignore_global bin; do 
    y="ln -f -s $repository_folder/$x $x"
    echo $y
    `$y`
